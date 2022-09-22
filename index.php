0acde1ef310e0e11f550cf148582106abbbd4873
Diana Nurfitra <89642724+ndiana13@users.noreply.github.com>
2022-09-22 15:15:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+     <title>SISKA | Sistem Informasi Surat Keputusan Akademik </title>
+
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+     <link rel="stylesheet" href="dist/css/adminlte.min.css">
+     <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+     <link rel="stylesheet" href="dist/css/font-awesome.min.css">
+     <link rel="stylesheet" href="dist/css/aos.css">
+     <link rel="stylesheet" href="dist/css/owl.carousel.min.css">
+     <link rel="stylesheet" href="dist/css/owl.theme.default.min.css">
+     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+     <link rel="shortcut icon" href="dist/img/pnc.png">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="dist/css/templatemo-digital-trend.css">
+
+</head>
+<body>
+
+     <!-- MENU BAR -->
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+              <i class="fa fa-envelope"></i>
+              S I S K A | P N C
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="#about" class="nav-link smoothScroll">Tentang</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#site-footer" class="nav-link smoothScroll">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="login/login.php" class="nav-link contact">Log in</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+     <!-- HERO -->
+     <section class="hero hero-bg d-flex justify-content-center align-items-center">
+               <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-6 col-md-10 col-12 d-flex flex-column justify-content-center align-items-center">
+                              <div class="hero-text">
+
+                                   <h1 class="text-grey" data-aos="fade-up">Selamat Datang di SISKA !</h1>
+
+                                   <h3 class="text-grey" data-aos="fade-up">Sistem Informasi Surat Keputusan Akademik Politeknik Negeri Cilacap</h3>
+
+                                   <a href="login/login.php" class="custom-btn btn-bg btn mt-3" data-aos="fade-up" data-aos-delay="100" ><i class="fas fa-arrow-circle-right black"></i></a> 
+
+                              </div>
+                        </div>
+
+                        <div class="col-lg-6 col-12">
+                          <div class="hero-image" data-aos="fade-up" data-aos-delay="300">
+
+                            <img src="dist/img/poltek.png" class="img-fluid" alt="working girl">
+                          </div>
+                        </div>
+
+                    </div>
+               </div>
+     </section>
+
+
+     <!-- ABOUT -->
+     <section class="about section-padding pb-0" id="about">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-lg-7 mx-auto col-md-10 col-12">
+                         <div class="about-info">
+
+                              <h2 class="mb-4" data-aos="fade-up">Sistem Informasi<strong> Surat Keputusan</strong> Akademik</h2>
+
+                              <p class="mb-0" data-aos="fade-up">Sistem Informasi <strong>Surat Keputusan</strong> Akademik merupakan sebuah sistem pengelolaan surat keputusan bidang akademik di Politeknik Negeri Cilacap.
+                              <br><br>Sistem ini <strong>menangani</strong> Surat Keputusan Mengajar, Surat Keputusan Dosen Wali dan Surat Keputusan Magang Industri.</p>
+                         </div>
+
+                         <div class="about-image" data-aos="fade-up" data-aos-delay="200">
+                         <br>
+                          <img src="dist/img/gedungpnc.jpg" class="img-fluid" alt="office" width="500" height="500">
+                        </div>
+                    </div>
+
+               </div>
+          </div>
+     </section>
+
+    <footer class="site-footer" id="site-footer">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-5 mx-lg-auto col-md-8 col-10">
+            <h1 class="text-white" data-aos="fade-up" data-aos-delay="100">Sistem Informasi <strong>Surat</strong> Keputusan Akademik.</h1>
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="200">
+            <h4 class="my-4">Contact Info</h4>
+
+            <p class="mb-1">
+              <i class="fa fa-phone mr-2 footer-icon"></i> 
+              +62 858 4242 5520
+            </p>
+
+            <p>
+              <a href="#">
+                <i class="fa fa-envelope mr-2 footer-icon"></i>
+                diananurfitra13@gmail.com
+              </a>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="300">
+            <h4 class="my-4">Location</h4>
+
+            <p class="mb-1">
+              <i class="fa fa-home mr-2 footer-icon"></i> 
+              Jl. Dr. Soetomo No.1, Karangcengis, Sidakaya, Kec. Cilacap Sel., Kabupaten Cilacap, Jawa Tengah 53212
+            </p>
+          </div>
+
+          <div class="col-lg-4 mx-lg-auto text-center col-md-8 col-12" data-aos="fade-up" data-aos-delay="400">
+            <p class="copyright-text">Copyright &copy; 2022 Politeknik Negeri Cilacap</p>
+          </div>
+
+          <div class="col-lg-3 mx-lg-auto col-md-6 col-12" data-aos="fade-up" data-aos-delay="600">
+          </div>
+
+        </div>
+      </div>
+    </footer>
+
+
+     <!-- SCRIPTS -->
+     <script src="dist/js/jquery.min.js"></script>
+     <script src="dist/js/bootstrap.min.js"></script>
+     <script src="dist/js/aos.js"></script>
+     <script src="dist/js/owl.carousel.min.js"></script>
+     <script src="dist/js/smoothscroll.js"></script>
+     <script src="dist/js/custom.js"></script>
+
+</body>
+</html>
